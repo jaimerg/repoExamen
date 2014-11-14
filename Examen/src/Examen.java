aab9cd0a01d475188893c0e09647dc4d880037e4
jaime <jaimerg21@gmail.com>
2014-11-14 16:55:07 +0100

    Primera subida de proyecto

---
+++ Examen/src/Examen.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Examen {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws Exception{
+		System.out.println("Dime tu nombre");
+		String nombre = br.readLine();
+		
+	}
+
+}
